eda1112500bd4edd4a5978849fe62b6e5613883a
gab5987 <99911394+gab5987@users.noreply.github.com>
2022-03-06 09:13:38 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_image.h>
+#include <allegro5/allegro_audio.h>
+#include "allegro5/allegro_acodec.h"
 #include "Movimentos_personagem.h"
 #include "Desenha_fundo.h"
 #include "Colisao.h"
@@ -15,14 +17,36 @@
   
 int main(){ 
 
- al_init(); al_init_image_addon(); if(!al_init()){return -1;}
+ al_init(); al_init_image_addon(); al_init_acodec_addon();
  bool done=false, col[4];
+
  ALLEGRO_BITMAP *prota  = al_load_bitmap("Personagens/Protagonista.bmp"); al_convert_mask_to_alpha(prota, al_map_rgb(255,0,255));
  ALLEGRO_BITMAP *fundo  = al_load_bitmap("Mapas/vila.bmp");
  ALLEGRO_BITMAP *colisao_1 = al_load_bitmap("Mapas/vila_col.bmp");
  ALLEGRO_BITMAP *over  = al_load_bitmap("Mapas/vila_ov.bmp"); al_convert_mask_to_alpha(over, al_map_rgb(0,0,0));
+
  ALLEGRO_TIMER *timer = NULL;
+
  ALLEGRO_DISPLAY *display = NULL;
+
+
+ALLEGRO_AUDIO_STREAM *musica_vila = al_load_audio_stream("soundtracks/vila.ogg", 1, 1024);
+//ALLEGRO_SAMPLE *musica_vila_sample = NULL;
+al_attach_audio_stream_to_mixer(musica_vila, al_get_default_mixer());
+al_set_audio_stream_playing(musica_vila, true);
+
+
+/*
+ALLEGRO_VOICE *voice; voice = al_create_voice(44100, ALLEGRO_AUDIO_DEPTH_INT16, ALLEGRO_CHANNEL_CONF_2);
+ALLEGRO_MIXER *mixer; mixer = al_create_mixer(44100, ALLEGRO_AUDIO_DEPTH_FLOAT32, ALLEGRO_CHANNEL_CONF_2);
+al_attach_mixer_to_voice(mixer, voice);
+al_reserve_samples(1);
+ALLEGRO_SAMPLE *musica_vila = al_load_sample("sounds/vila.ogg");
+ALLEGRO_SAMPLE_INSTANCE *musica_vila_instancia = al_create_sample_instance(musica_vila);
+al_set_sample_instance_playmode(musica_vila_instancia, ALLEGRO_PLAYMODE_LOOP);
+al_attach_sample_instance_to_mixer(musica_vila_instancia, al_get_default_mixer());
+*/
+
  ALLEGRO_EVENT_QUEUE *event_queue = NULL;
  ALLEGRO_COLOR i;
 
@@ -31,6 +55,7 @@ int main(){
 
   al_init_primitives_addon();
 	al_install_keyboard();
+  al_install_audio();
 
 	event_queue = al_create_event_queue();
 
@@ -44,6 +69,9 @@ int main(){
       ALLEGRO_EVENT ev;
 		  al_wait_for_event(event_queue, &ev);
       if(ev.keyboard.keycode == ALLEGRO_KEY_ESCAPE){done = true;}
+
+      //al_play_sample_instance(musica_vila_instancia);
+      //al_play_sample(musica_vila_sample, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_ONCE, NULL);
 
       if(ev.type == ALLEGRO_EVENT_KEY_DOWN){
 			  switch(ev.keyboard.keycode){
