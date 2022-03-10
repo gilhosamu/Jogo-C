248466f7be20c5923eb1e24a42a069e5643dc0c0
Gabriel Novalsky <99911394+gab5987@users.noreply.github.com>
2022-03-10 18:30:32 -0300

    alguem esqueceu de linkar os arquivos das funções né lucas

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 echo "Compilando arquivo main.c"
 
-gcc main.c -o main $(pkg-config allegro-5 allegro_main-5 allegro_font-5 allegro_primitives-5 allegro_image-5 allegro_audio-5 allegro_acodec-5  --libs --cflags)
+gcc main.c Colisao.c Desenha_fundo.c Movimentos_personagens.c NPCs.c -o main $(pkg-config allegro-5 allegro_main-5 allegro_font-5 allegro_primitives-5 allegro_image-5 allegro_audio-5 allegro_acodec-5  --libs --cflags)
 
 ./main
