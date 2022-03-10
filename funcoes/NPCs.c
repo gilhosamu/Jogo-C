a2afd3cf3065b4c5a23d1c8161aa2ef2a2a97795
gab5987 <99911394+gab5987@users.noreply.github.com>
2022-03-09 21:38:52 -0300

    Add files via upload

--- funcoes/NPCs.c
+++ funcoes/NPCs.c
@@ -1,17 +1,46 @@
 #include "funcoes.h"
 
-void NPC(){
-    struct npc{
+struct def{
         int vidas;
         int causa_dano;
         int px; int py;
         int morto;
     };
-    struct mago_careca{
-        struct npc ;
+    struct NPC{
+        struct def mago_chapeu;
+        struct def mago_careca;
+        struct def skeleton;
+        struct def caveira;
+        struct def zumbi;
     };
-    struct mago_chapeu{};
-    struct skeleton{};
-    struct caveira{};
-    struct zumbi{};
+
+void NPC(int *mapa_atual){
+    switch(*mapa_atual){
+        case 0:
+        break;
+        case 1:
+        break;
+        case 2:
+        break;
+        case 3:
+        break;
+        case 4:
+        break;
+        case 5:
+        break;
+        case 6:
+        break;
+        case 7:
+        break;
+        case 8:
+        break;
+        case 9:
+        break;
+        case 10:
+        break;
+        case 11:
+        break;
+
+    }
+    
 }
