4bfaa32135252ee5079b7b11a568468a46a14e2c
Gabriel Novalsky <99911394+gab5987@users.noreply.github.com>
2022-03-11 17:25:51 -0300

    bugs

--- funcoes/Colisao.c
+++ funcoes/Colisao.c
@@ -1,69 +1,48 @@
 #include "funcoes.h"
 
-struct npc{
-    int vidas;
-    int causa_dano;
-    int x; int y;
-    int morto;
-    int aut; int pos; int passo;
-};
-struct npc v10;
-
-void NPC(int *mapa_atual, int *count, int load_pos){
-    ALLEGRO_BITMAP *V10  = al_load_bitmap("Personagens/villagers/v10.bmp");
-
-    if(load_pos == 0){
-         v10.x = 530; v10.y = 82;
-    }
-    printf("\n %d  %d  %d  %d", v10.x, v10.y, *count, v10.pos);
-
+void Troca_mapa(int *mapa_atual, int *pos_x, int  *pos_y, int *load_pos){
     switch(*mapa_atual){
         case 0:
+        if(*pos_x == 766){*mapa_atual = 1; *pos_x = 0; *load_pos = 0;}
+        if(*pos_y == 0){*mapa_atual = 2; *pos_y = 560; *load_pos = 0;}
         break;
 
         case 1:
-        if(*count == 0 || *count == 9 || *count == 18){
-            if(v10.aut == 0){
-                v10.pos = v10.pos +1;
-                if(v10.pos == 3){v10.pos = 0;}
-                if(v10.y == 416){v10.aut = 1;}
-                v10.y = v10.y +1;
-            }
-            if(v10.aut == 1){
-                v10.pos = v10.pos +1;
-                if(v10.pos == 3){v10.pos = 0;}
-                if(v10.y == 80){v10.aut = 0;}
-                v10.y = v10.y -1;
-            }
-        }
-        switch(v10.pos){
-            case 0: al_draw_bitmap_region(V10, 0,96*v10.aut,32,32,v10.x,v10.y,0);
-            case 1: al_draw_bitmap_region(V10, 32,96*v10.aut,32,32,v10.x,v10.y,0);
-            case 2: al_draw_bitmap_region(V10, 64,96*v10.aut,32,32,v10.x,v10.y,0);
-        }
+        if(*pos_x == 0){*mapa_atual = 0; *pos_x = 760; *load_pos = 0;}
+        else if(*pos_y == 0){*mapa_atual = 3; *pos_y = 560; *load_pos = 0;}
         break;
 
-        case 2:
+        case 2: 
+        if(*pos_x == 766){*mapa_atual = 3; *pos_x = 32; *load_pos = 0;}
+        if(*pos_y == 0){*mapa_atual = 5; *pos_y = 560; *load_pos = 0;}
+        if(*pos_y == 566){*mapa_atual = 0; *pos_y = 32; *load_pos = 0;}
         break;
+
         case 3:
-        break;
-        case 4:
-        break;
-        case 5:
-        break;
-        case 6:
-        break;
-        case 7:
-        break;
-        case 8:
-        break;
-        case 9:
-        break;
-        case 10:
-        break;
-        case 11:
+        if(*pos_x == 0){*mapa_atual = 2; *pos_x = 760; *load_pos = 0;}
+        if(*pos_x == 766){*mapa_atual = 4; *pos_x = 32; *load_pos = 0;}
+        if(*pos_y == 0){*mapa_atual = 6; *pos_y = 560; *load_pos = 0;}
+        if(*pos_y == 566){*mapa_atual = 1; *pos_y = 32; *load_pos = 0;}
         break;
 
+        case 4:
+        if(*pos_x == 0){*mapa_atual = 3; *pos_x = 760; *load_pos = 0;}
+        if(*pos_x == 766){*mapa_atual = 8; *pos_x = 100; *load_pos = 0;}
+        break;
+
+        case 5:
+        if(*pos_x == 766){*mapa_atual = 6; *pos_x = 32; *load_pos = 0;}
+        if(*pos_y == 566){*mapa_atual = 3; *pos_y = 32; *load_pos = 0;}
+        break;
+
+        case 6: 
+        if(*pos_x == 0){*mapa_atual = 5; *pos_x = 760; *load_pos = 0;}
+        if(*pos_x == 766){*mapa_atual = 7; *pos_x = 32; *load_pos = 0;}
+        if(*pos_y == 566){*mapa_atual = 3; *pos_y = 32; *load_pos = 0;}
+        break;
+
+        case 7:
+        if(*pos_x == 0){*mapa_atual = 6; *pos_x = 760; *load_pos = 0;}
+        break;
     }
-    
 }
